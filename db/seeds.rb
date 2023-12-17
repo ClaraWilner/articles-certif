aa3de27dcefa194b2fca589daafdfe544c1976d3
clara wilner <clara.luna.wilner@gmail.com>
2023-12-17 22:49:15 +0200

    premier essai

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+User.destroy_all
+Article.destroy_all
+
+puts "Creating users..."
+clara = User.create({username: "clara", email: "clara.luna@gmail.com", password: "azerty"})
+roi =  User.create({username: "roi", email: "roi.eden@gmail.com", password: "azerty"})
+
+"Users created"
+
+
+
+
+article_1 = Article.create({title: "kundera", content: "HJMKJHMKJHkzjhlkjsrhvlkjsrhlkjqklrjh", rating: 0, category: "Literature"})
+article_1.user = clara
+puts "article_1 created"
+
+article_2 = Article.create({title: "garcia marquez", content: "lkjqrblksjvlqkjhjh", rating: 0, category: "Literature"})
+article_2.user = roi
+puts "article_2 created"
+
+puts "Finished"
